fdb21b6ddeb240b7090aa8cf17c0b35f9056b32c
MeshCollider <dobsonsa68@gmail.com>
2015-09-08 20:08:53 +1200

    Update and rename signature_seller.php to buysigs.php

--- inc/plugins/buysigs.php
+++ inc/plugins/buysigs.php
@@ -20,9 +20,9 @@ if (!defined("IN_MYBB")) {
 }
 
 // Hooks
-$plugins->add_hook("member_profile_start", "signature_seller_member_profile_start");
+$plugins->add_hook("member_profile_start", "buysigs_member_profile_start");
 
-function signature_seller_info() {
+function buysigs_info() {
 	return array(
 		"name"		=> "Omnicoin Signature Seller",
 		"description"	=> "This plugin allows automatic management of signature sales for merchants",
@@ -35,25 +35,53 @@ function signature_seller_info() {
 	);
 }
 
-function signature_seller_install() {
+function buysigs_install() {
 	//Called whenever a plugin is installed by clicking the "Install" button in the plugin manager.
 	//It is common to create required tables, fields and settings in this function.	
 }
 
-function signature_seller_is_installed() {
+function buysigs_is_installed() {
 	//Called on the plugin management page to establish if a plugin is already installed or not.
 	//This should return TRUE if the plugin is installed (by checking tables, fields etc) or FALSE if the plugin is not installed.
   	return false;
 }
 
-function signature_seller_uninstall() {
+function buysigs_uninstall() {
 	//Called whenever a plugin is to be uninstalled. This should remove ALL traces of the plugin from the installation (tables etc). If it does not exist, uninstall button is not shown.
 }
 
-function signature_seller_activate() {
+function buysigs_activate() {
 	//Called whenever a plugin is activated via the Admin CP. This should essentially make a plugin "visible" by adding templates/template changes, language changes etc.
 }
 
-function signature_seller_deactivate() {
+function buysigs_deactivate() {
 	//Called whenever a plugin is deactivated. This should essentially "hide" the plugin from view by removing templates/template changes etc. It should not, however, remove any information such as tables, fields etc - that should be handled by an _uninstall routine. When a plugin is uninstalled, this routine will also be called before _uninstall() if the plugin is active.
 }
+
+/*
+Add function to prevent an active status sold signature to be altered.
+
+
+----------------------------
+Database Scheme:
+
+Table Name:sigsales (for the listing of sales)
+Columns:
+id
+uid
+price (in omc of course)
+length (will be sold in days)
+status (0=open listing, 1=sold, 2=expired)
+
+Table Name: sigbuys (for the sales, saving and replacing of sigs)
+Columns:
+id
+uid_seller
+uid_buyer
+oldsig
+newsig
+status (0=active, 1=expired)
+buydate (use unix dateline time)
+expdate (use unix dateline time)
+txnid (omc sales transaction id)
+*/
